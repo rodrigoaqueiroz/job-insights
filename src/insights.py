a9b86c31a7a5c6c516479e6cd6e4fc33fdd5f077
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2022-03-22 20:45:13 -0300

    feat: req08 - matches_salary_range

--- src/insights.py
+++ src/insights.py
@@ -38,7 +38,7 @@ def get_max_salary(path):
     file = read(path)
     max_salary = []
     for salary in file:
-        if salary['max_salary'].isdigit():
+        if salary["max_salary"].isdigit():
             max_salary.append(float(salary["max_salary"]))
     return max(max_salary)
 
@@ -47,35 +47,27 @@ def get_min_salary(path):
     file = read(path)
     min_salary = []
     for salary in file:
-        if salary['min_salary'].isdigit():
+        if salary["min_salary"].isdigit():
             min_salary.append(float(salary["min_salary"]))
     return min(min_salary)
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        if job["min_salary"] <= int(salary) <= job["max_salary"]:
+            return True
+        elif job["min_salary"] > job["max_salary"]:
+            raise ValueError
+        else:
+            return False
+    except(TypeError, KeyError):
+        raise ValueError
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+# referência: para o 'raise' (a forma python de mandar um throw)
+# https://stackoverflow.com/questions/2052390/manually-raising-throwing-an-exception-in-python
+# referência para o erro 'do not use bare except'
+# https://www.flake8rules.com/rules/E722.html
 
 
 def filter_by_salary_range(jobs, salary):
